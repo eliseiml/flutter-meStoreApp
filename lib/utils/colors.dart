7f086aec8527019df8d4eb7c052355d99088e98c
eliseiml.ml <eliseiml.ml@yandex.ru>
2021-06-18 12:38:50 +0300

    app setup, localization, splash screen

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Color geyser = Color(0xFFCFD8DF);
+Color seaShell = Color(0xFFF2F0F0);
+Color alizarinCrimson = Color(0xFFDA2525);
+Color grey = Color(0xFF8A8989);
+Color havelockBlue = Color(0xFF438BCD);
+Color rhino = Color(0xFF2E3E5C);
+Color black = Color(0xFF000000);
+Color concrete = Color(0xFFF3F3F3);
+Color white = Color(0xFFFFFFFF);
+Color malachite = Color(0xFF1DBD0F);
